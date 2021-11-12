836f6817362740efd50a4a7bd1f6b1e2ceaaf416
sneh3195 <snehjainnsk@gmail.com>
2021-11-12 12:26:05 +0530

    UseCase-9 : multiple componies can check wages calculation of their employee

--- src/Com/Employeewages/Employee.java
+++ src/Com/Employeewages/Employee.java
@@ -1,43 +1,53 @@
 package Com.Employeewages;
 
+import java.util.Scanner;
+
 public class Employee {
  
-	public static final int IS_PART_TIME = 1;
-	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int MAX_HOUR_IN_MONTH = 100;
-	public static final int NO_OF_WORKING_DAYS = 21;
-
-      public int calculateEmpwage() {
+public void computeEmployeeWage(String name, int no_of_days, int max_hours_in_month, int wages_per_hour) {	
 		
-    	  int empHrs = 0;
-    	  int totalHrsInMonth = 0;
-    	  int totalWorkingDaysInMonth = 0;
-    	  
-    	  while(totalHrsInMonth<=MAX_HOUR_IN_MONTH && totalWorkingDaysInMonth<NO_OF_WORKING_DAYS) {
-    		  
-    		  int empStatus = (int)Math.floor((Math.random()*10)%3);
-    		  switch (empStatus) {
-			case IS_PART_TIME:
-				empHrs = 4;
+		Scanner input = new Scanner(System.in);
+		
+		final int IS_PART_TIME = 1;
+		final int IS_FULL_TIME = 2;
+		final int PART_TIME_HOURS = 4;
+		final int FULL_TIME_HOURS = 8;
+		
+		int wage = 0;    
+		int days = 0;    
+		int hours = 0;   
+		
+		while (days < no_of_days && hours < max_hours_in_month) {
+			int attendance = (int)Math.floor( (Math.random() * 10) % 3);
+			
+			switch (attendance) {
+			case 0:
 				break;
-			case IS_FULL_TIME:	
-				empHrs = 8;
-				break;
-			default:
-				empHrs = 0;
-				//break;
+				
+			case IS_PART_TIME: 
+					if (hours + PART_TIME_HOURS > 100) {
+						break;
+					}
+					hours += PART_TIME_HOURS;
+					days++;
+					break;
+			case IS_FULL_TIME:
+					if (hours + FULL_TIME_HOURS > 100) {
+						break;
+					}
+					hours += FULL_TIME_HOURS;
+					days++;
+					break;				
 			}
-    		  totalHrsInMonth += empHrs;
-    		  System.out.println("Total Days :" +totalWorkingDaysInMonth);
-    	  }
-    	  
-    	  int totalWage = totalHrsInMonth * EMP_RATE_PER_HOUR;
-    	  System.out.println("Total Employee wages for the month is : "+totalWage);
-    	  
-    	  return totalWage;
-    	  
-      }
+		}
+		
+		wage = hours * wages_per_hour;
+		
+		System.out.println(" ****** " + name + " ******");
+		System.out.println(" TOTAL WAGES= " + wage);
+		System.out.println(" TOTAL WORKING DAYS = " + days);
+		System.out.println(" TOTAL WORKING HOURS = " + hours);	
+	}
 
 
 
